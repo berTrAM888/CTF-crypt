419dd1fb690658137e14070c5726a4938cd03050
bertram888 <928921254@qq.com>
2017-06-20 03:13:16 +0800

    add modules fence_crypt

---
+++ modules/fence.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import re
+
+def fence_crypt(string,option,num=1):
+	if option=='encode':
+		string=left_words(string)
+	else:
+		fence_decrypt(string)
+
+def fence_decrypt(e):
+    elen = len(e)
+    field=[]
+    for i in range(2,elen):
+        if(elen%i==0):
+            field.append(i)
+
+    for f in field:
+        b = elen / f
+        result = {x:'' for x in range(b)}
+        for i in range(elen):
+            a = i % b;
+            result.update({a:result[a] + e[i]})
+        d = ''
+        for i in range(b):
+            d = d + result[i]
+        print d.lower()
+		
+
+def left_words(string):
+	return ''.join(re.findall('[a-zA-Z0-9]+',string))
